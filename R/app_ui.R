7ae3ad1d4d1bff8f69dc1805732ae6efc4a98d14
codydangerfield-usfs <cody.dangerfield@usda.gov>
2026-07-29 13:23:56 -0600

    Align browse button UI markup with working script behavior

--- R/app_ui.R
+++ R/app_ui.R
@@ -96,7 +96,12 @@ ui <- page_fillable(
         
         tags$label("Root Folder Path", class = "form-label", `for` = "root_dir"),
         div(class = "input-group mb-3",
-            actionButton("browse_root", "Browse...", class = "btn btn-default"),
+            tags$button(
+              id = "browse_root",
+              type = "button",
+              class = "btn btn-default action-button",
+              "Browse..."
+            ),
             tags$input(
               id = "root_dir",
               type = "text",
@@ -108,7 +113,12 @@ ui <- page_fillable(
         
         tags$label("Master Database File", class = "form-label", `for` = "master_db"),
         div(class = "input-group mb-3",
-            actionButton("browse_db", "Browse...", class = "btn btn-default"),
+            tags$button(
+              id = "browse_db",
+              type = "button",
+              class = "btn btn-default action-button",
+              "Browse..."
+            ),
             tags$input(
               id = "master_db",
               type = "text",
@@ -120,7 +130,12 @@ ui <- page_fillable(
         
         tags$label("KCP Directory Name", class = "form-label", `for` = "kcp_dir"),
         div(class = "input-group mb-3",
-            actionButton("browse_kcp", "Browse...", class = "btn btn-default"),
+            tags$button(
+              id = "browse_kcp",
+              type = "button",
+              class = "btn btn-default action-button",
+              "Browse..."
+            ),
             tags$input(
               id = "kcp_dir",
               type = "text",
