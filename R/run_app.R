11fcb61d8782b940684d907f48bff52511321683
Dangerfield <12001003496953@FEDIDCARD.GOV>
2026-07-29 16:53:45 -0600

    Refactor package to execute single-file Shiny script using system.file

--- R/run_app.R
+++ R/run_app.R
@@ -3,7 +3,7 @@
 #' @param launch.browser Logical; passed to shiny::runApp.
 #' @param ... Additional arguments passed to shiny::runApp.
 #' @export
-run_app <- function(launch.browser = TRUE, ...) {
+run_app <- function(launch.browser = getOption("shiny.launch.browser", interactive()), ...) {
   if (!requireNamespace("rFVS", quietly = TRUE)) {
     stop(
       "Package 'rFVS' is required. Install it with:\n",
@@ -12,16 +12,20 @@ run_app <- function(launch.browser = TRUE, ...) {
     )
   }
 
-  if (exists("register_workflow_assets", mode = "function")) {
-    register_workflow_assets()
+  app_dir <- system.file("app", package = "FVSBatchProcessor")
+  if (app_dir == "") {
+    stop("Could not find app directory. Try re-installing `FVSBatchProcessor`.", call. = FALSE)
   }
+
+  # Forward the user's current working directory into the Shiny app options
+  shinyOptions(FVS_USER_WD = getwd())
 
   # Increase maximum upload size to 10GB for very large database/file transfers
   opt <- options(shiny.maxRequestSize = 10000 * 1024^2)
   on.exit(options(opt), add = TRUE)
 
   shiny::runApp(
-    shiny::shinyApp(ui = ui, server = server),
+    app_dir,
     launch.browser = launch.browser,
     ...
   )
